8bf39ca7b1fa40f65f740c0758a916bdce195ce3
William L <w-lindvall@outlook.com>
2019-11-30 21:02:32 -0800

    fix avatar color when loading

--- client/src/components/Avatar/Avatar.styles.js
+++ client/src/components/Avatar/Avatar.styles.js
@@ -1,11 +1,11 @@
 import {makeStyles} from '@material-ui/core/styles'
-import {red} from '@material-ui/core/colors'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     borderRadius: '50%',
-    backgroundColor: red[500],
+    backgroundColor: theme.palette.primary.main,
+    color: 'transparent',
   },
-})
+}))
 
 export default useStyles
