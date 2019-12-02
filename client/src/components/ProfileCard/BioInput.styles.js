1530153fb46089ceda6592638eddad88479e45c0
William L <w-lindvall@outlook.com>
2019-12-01 20:44:09 -0800

    tweak profile card width

--- client/src/components/ProfileCard/BioInput.styles.js
+++ client/src/components/ProfileCard/BioInput.styles.js
@@ -7,6 +7,9 @@ const useStyles = makeStyles(theme => ({
   input: {
     width: '100%',
     marginBottom: theme.spacing(1),
+    [theme.breakpoints.up('sm')]: {
+      width: '345px',
+    },
   },
   submitBtn: {
     display: 'block',
--- client/src/components/ProfileCard/ProfileCard.styles.js
+++ client/src/components/ProfileCard/ProfileCard.styles.js
@@ -2,10 +2,12 @@ import {makeStyles} from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 345,
     marginRight: 'auto',
     marginLeft: 'auto',
     marginBottom: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      width: '650px',
+    },
   },
   title: {
     fontSize: '2rem',
