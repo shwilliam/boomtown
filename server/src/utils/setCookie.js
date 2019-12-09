d468768987fb8b7f7a759697f5c51326cdb70f5c
William L <w-lindvall@outlook.com>
2019-12-08 22:21:01 -0800

    expire cookie after 30 min

--- server/src/utils/setCookie.js
+++ server/src/utils/setCookie.js
@@ -2,7 +2,7 @@ const setCookie = ({tokenName, token, res}) => {
   res.cookie(tokenName, token, {
     // httpOnly: true,
     secure: Boolean(process.env.NODE_ENV === 'production'),
-    maxAge: 7200000, // 2 hrs
+    maxAge: 1800000, // 30 min
   })
 }
 
